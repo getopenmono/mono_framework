d22ce0bfc9331e9f9d343bb91c19a4223b4fa9c9
Kristoffer Andersen <ka@openmono.com>
2015-11-15 12:39:25 +0100

    unit tests for queue and string classes

---
+++ tests/queue_test.cpp
@@ -0,0 +1,123 @@
+//
+//  heap_queue_test.cpp
+//  heapQueueTest
+//
+//  Created by Kristoffer Andersen on 10/11/2015.
+//  Copyright © 2015 Monolit ApS. All rights reserved.
+//
+
+#include "catch.hpp"
+#include "../queue.h"
+
+using namespace mono;
+
+class Number : public IQueueItem
+{
+public:
+    int n;
+    
+    Number(int num)
+    {
+        n = num;
+    }
+};
+
+SCENARIO("Heap based Queue works","[queue]")
+{
+    GIVEN("An empty queue")
+    {
+        GenericQueue<Number> queue;
+        
+        
+        WHEN("one item is added")
+        {
+            
+            queue.Enqueue(new Number(45));
+            
+            THEN("the length must be 1")
+            {
+                REQUIRE(queue.Length() == 1);
+            }
+            
+            
+            THEN("we should dequeue that same object")
+            {
+                Number *num = (Number*) queue.Dequeue();
+                
+                REQUIRE(num != NULL);
+                REQUIRE(num->n == 45);
+            }
+        }
+        
+        WHEN("4 items are added")
+        {
+            queue.Enqueue(new Number(1));
+            queue.Enqueue(new Number(2));
+            queue.Enqueue(new Number(3));
+            queue.Enqueue(new Number(4));
+            
+            THEN("length should be 4")
+            {
+                REQUIRE(queue.Length() == 4);
+            }
+            
+            WHEN("we dequeue 2")
+            {
+                Number *n1 = (Number*) queue.Dequeue();
+                Number *n2 = (Number*) queue.Dequeue();
+                
+                THEN("the dequeued must must first added")
+                {
+                    REQUIRE(n1 != NULL);
+                    REQUIRE(n1->n == 1);
+                    
+                    REQUIRE(n2 != NULL);
+                    REQUIRE(n2->n == 2);
+                }
+                
+                THEN("length must be 2")
+                {
+                    REQUIRE(queue.Length() == 2);
+                }
+                
+                WHEN("we enqueue 1 more")
+                {
+                    queue.Enqueue(new Number(5));
+                    
+                    THEN("length must be 3")
+                    {
+                        REQUIRE(queue.Length() == 3);
+                    }
+                    
+                    WHEN("we dequeue all 3 remaining")
+                    {
+                        Number *n1 = (Number*) queue.Dequeue();
+                        Number *n2 = (Number*) queue.Dequeue();
+                        Number *n3 = (Number*) queue.Dequeue();
+                        
+                        THEN("objects must match")
+                        {
+                            REQUIRE(n1 != NULL);
+                            REQUIRE(n1->n == 3);
+                            
+                            REQUIRE(n2 != NULL);
+                            REQUIRE(n2->n == 4);
+                            
+                            REQUIRE(n3 != NULL);
+                            REQUIRE(n3->n == 5);
+                        }
+                        
+                        THEN("length must be 0")
+                        {
+                            REQUIRE(queue.Length() == 0);
+                        }
+                    }
+                }
+                
+            }
+            
+            
+        }
+        
+    }
+}
---
+++ tests/string_test.cpp
@@ -0,0 +1,54 @@
+//
+//  string_test.cpp
+//  mnStringTest
+//
+//  Created by Kristoffer Andersen on 13/11/2015.
+//  Copyright © 2015 Monolit ApS. All rights reserved.
+//
+
+#include "catch.hpp"
+#include "../mn_string.h"
+
+SCENARIO("test the mono string class works")
+{
+    GIVEN("a string with const chars")
+    {
+        const char *constChar = "hej, jeg er konstant!";
+        mono::String conStr(constChar);
+        
+        THEN("it should not be malloced")
+        {
+            REQUIRE(conStr.malloced == false);
+        }
+        
+        WHEN("we create a copy of the string")
+        {
+            mono::String copy = conStr;
+            
+            THEN("the copy should be a ref to the const char")
+            {
+                REQUIRE(copy.stringData == conStr.stringData);
+            }
+        }
+    }
+    
+    GIVEN("a dynamic string")
+    {
+        mono::String dynStr = mono::String::Format("Hej jeg er en %s streng","dynamisk");
+        
+        THEN("it should be malloced")
+        {
+            REQUIRE(dynStr.malloced == true);
+        }
+        
+        WHEN("we make a new copy")
+        {
+            mono::String copy = dynStr;
+            
+            THEN("it shoul dref the original")
+            {
+                REQUIRE(dynStr.stringData == copy.stringData);;
+            }
+        }
+    }
+}
