310df273033e66ae9700967c712f0f48b85d8934
Jens Peter Secher <jp@openmono.com>
2016-02-08 16:35:33 +0100

    BackgroundView class to just set background color.

--- mono.h
+++ mono.h
@@ -1,60 +1,49 @@
-//
-//  mono.h
-//  Mono
-//
-//  Created by Kristoffer Andersen on 27/05/15.
-//
-//
-
-#ifndef spiTest_mono_h
-#define spiTest_mono_h
+// This software is part of OpenMono, see http://developer.openmono.com
+// Released under the MIT license, see LICENSE.txt
+#ifndef __mono__mono__
+#define __mono__mono__
 
 #include <stdint.h>
 #include <managed_pointer.h>
 
-#include <consoles.h>
-#include <application_controller_interface.h>
 #include <application_context_interface.h>
+#include <application_controller_interface.h>
 #include <application_run_loop_task_interface.h>
-#include <queue_interrupt.h>
-#include <mn_timer.h>
-#include <mn_string.h>
-#include <queue.h>
-#include <regex.h>
-
-#include <point.h>
-#include <size.h>
-#include <rect.h>
 #include <circle.h>
-
 #include <color.h>
-#include <display_painter.h>
-
-#include <touch_system_interface.h>
-
-#include <display/ui/view.h>
-#include <display/ui/responder_view.h>
-#include <display/ui/console_view.h>
-#include <display/ui/image_view.h>
-#include <display/ui/text_label_view.h>
-#include <display/ui/button_view.h>
+#include <consoles.h>
 #include <display/ui/animator.h>
-#include <display/ui/touch_calibrator.h>
-#include <display/ui/progress_bar_view.h>
-#include <display/ui/status_indicator_view.h>
+#include <display/ui/background_view.h>
+#include <display/ui/button_view.h>
+#include <display/ui/console_view.h>
 #include <display/ui/graph_view.h>
-
-#include <media/image.h>
+#include <display/ui/image_view.h>
+#include <display/ui/progress_bar_view.h>
+#include <display/ui/responder_view.h>
+#include <display/ui/status_indicator_view.h>
+#include <display/ui/text_label_view.h>
+#include <display/ui/touch_calibrator.h>
+#include <display/ui/view.h>
+#include <display_painter.h>
 #include <media/bmp_image.h>
+#include <media/image.h>
+#include <mn_digital_out.h>
+#include <mn_string.h>
+#include <mn_timer.h>
+#include <point.h>
+#include <queue.h>
+#include <queue_interrupt.h>
+#include <rect.h>
+#include <regex.h>
+#include <size.h>
+#include <touch_system_interface.h>
 
 #ifndef EMUNO
 #include <dns_resolver.h>
 #include <http_client.h>
 #endif
 
-#include <mn_digital_out.h>
-
 namespace mono {
 }
 
-#endif
+#endif // __mono__mono__
