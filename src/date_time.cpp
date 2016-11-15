acc31ce45f65260d1f04d7d1dbe23fbd2bf2296d
Kristoffer Andersen <ka@openmono.com>
2016-11-15 15:12:37 +0100

    Lots of updates to DateTime class. RTC not incorporated yet

--- src/date_time.cpp
+++ src/date_time.cpp
@@ -2,8 +2,27 @@
 // Released under the MIT license, see LICENSE.txt
 
 #include "date_time.h"
+#include "regex.h"
+#include <stdio.h>
 
 using namespace mono;
+
+const uint8_t DateTime::DaysPerMonth[13] =
+{
+    0,
+    31,
+    28,
+    31,
+    30,
+    31,
+    30,
+    31, //july
+    31, //aug
+    30,
+    31,
+    30,
+    31 //dec
+};
 
 // MARK: Public Constructors
 
@@ -11,27 +30,336 @@ DateTime::DateTime()
 {
     secs = mins = hours = day = month -1;
     year = 1970;
+    
+    type = UNKNOWN_TIME_ZONE;
 }
 
 DateTime::DateTime(uint16_t years, uint8_t months, uint8_t days,
-                   uint8_t hours, uint8_t minutes, uint8_t seconds)
+                   uint8_t hours, uint8_t minutes, uint8_t seconds, TimeTypes zone)
 {
     secs = trim(seconds, 0, 59);
     mins = trim(minutes, 0, 59);
     this->hours = trim(hours, 0, 23);
     day = trim(days, 1, 31);
     month = trim(months, 1, 12);
+    year = years;
+    leapYear = isLeapYear(years);
+    type = zone;
+}
+
+DateTime::DateTime(const DateTime &other)
+{
+    secs = other.secs;
+    mins = other.mins;
+    this->hours = other.hours;
+    day = other.day;
+    month = other.month;
+    year = other.year;
+    type = other.type;
+    leapYear = other.leapYear;
+}
+
+DateTime &DateTime::operator=(const DateTime &other)
+{
+    secs = other.secs;
+    mins = other.mins;
+    this->hours = other.hours;
+    day = other.day;
+    month = other.month;
+    year = other.year;
+    type = other.type;
+    leapYear = other.leapYear;
+
+    return *this;
 }
 
 // MARK: Public Methods
 
-String DateTime::ToString() const
+String DateTime::toString() const
 {
-    return String::Format("%u-%i-%i %i:%i:%i",year,month,day,hours,mins,secs);
+    return String::Format("%04d-%02d-%02d %02d:%02d:%02d",year,month,day,hours,mins,secs);
 }
 
+String DateTime::toISO8601() const
+{
+    const char *timeZone;
+    switch (type) {
+        case UTC_TIME_ZONE:
+            timeZone = "Z";
+            break;
+        case LOCAL_TIME_ZONE:
+        case UNKNOWN_TIME_ZONE:
+        default:
+            timeZone = "";
+            break;
+    }
+
+    return String::Format("%04d-%02d-%02dT%02d:%02d:%02d%s",year,month,day,hours,mins,secs,timeZone);
+}
+
+bool DateTime::isValid() const
+{
+    return month > 0;
+}
+
+DateTime DateTime::toUtcTime() const
+{
+    if (type == UTC_TIME_ZONE)
+        return *this;
+    else if (type == LOCAL_TIME_ZONE)
+        return DateTime(year, month, day, hours+LocalTimeZoneHourOffset, mins, secs, UTC_TIME_ZONE);
+    else
+        return *this;
+
+}
+
+// MARK: Date / Time manipulation
+
+DateTime DateTime::addSeconds(int seconds) const
+{
+    DateTime other(*this);
+    if (seconds > 0)
+    {
+        for (int s=0; s<seconds; s++) {
+            other.incrementSecond();
+        }
+    }
+
+    return other;
+}
+
+DateTime DateTime::addMinutes(int minutes) const
+{
+    DateTime other(*this);
+    if (minutes > 0)
+    {
+        for (int m=0; m<minutes; m++) {
+            other.incrementMinute();
+        }
+    }
+
+    return other;
+}
+
+DateTime DateTime::addHours(int hrs) const
+{
+    DateTime other(*this);
+    if (hrs > 0)
+    {
+        for (int h=0; h<hrs; h++) {
+            other.incrementHour();
+        }
+    }
+
+    return other;
+}
+
+DateTime DateTime::addDays(int days) const
+{
+    DateTime other(*this);
+    if (days > 0)
+    {
+        for (int d=0; d<days; d++) {
+            other.incrementDayOfMonth();
+        }
+    }
+
+    return other;
+}
+
+//DateTime DateTime::addMonths(int months) const
+//{
+//    DateTime other(*this);
+//    if (months > 0)
+//    {
+//        for (int M=0; M<months; M++) {
+//            other.incrementMonth();
+//        }
+//    }
+//
+//    return other;
+//}
+//
+//DateTime DateTime::addYears(int years) const
+//{
+//    DateTime other(*this);
+//    if (years > 0)
+//    {
+//        for (int y=0; y<years; y++) {
+//            other.incrementYear();
+//        }
+//    }
+//
+//    return other;
+//}
+
+// MARK: Operator overloads
+
+bool DateTime::operator==(const DateTime &other) const
+{
+    DateTime utcSelf = this->toUtcTime();
+    DateTime utcOther = other.toUtcTime();
+
+    return  utcSelf.secs == utcOther.secs &&
+            utcSelf.mins == utcOther.mins &&
+            utcSelf.hours == utcOther.hours &&
+            utcSelf.day == utcOther.day &&
+            utcSelf.month == utcOther.month &&
+            utcSelf.year == utcOther.year &&
+            utcSelf.leapYear == utcOther.leapYear;
+}
+
+bool DateTime::operator!=(const mono::DateTime &other) const
+{
+    return !(this->operator==(other));
+}
+
+bool DateTime::operator>(const mono::DateTime &other) const
+{
+    DateTime utcSelf = this->toUtcTime();
+    DateTime utcOther = other.toUtcTime();
+
+    return  utcSelf.year > utcOther.year ||
+            utcSelf.month > utcOther.month ||
+            utcSelf.day > utcOther.day ||
+            utcSelf.hours > utcOther.hours ||
+            utcSelf.mins > utcOther.mins ||
+            utcSelf.secs > utcOther.secs;
+}
+
+bool DateTime::operator<(const mono::DateTime &other) const
+{
+    DateTime utcSelf = this->toUtcTime();
+    DateTime utcOther = other.toUtcTime();
+
+    return  utcSelf.year < utcOther.year ||
+            utcSelf.month < utcOther.month ||
+            utcSelf.day < utcOther.day ||
+            utcSelf.hours < utcOther.hours ||
+            utcSelf.mins < utcOther.mins ||
+            utcSelf.secs < utcOther.secs;
+}
+
+bool DateTime::operator>=(const mono::DateTime &other) const
+{
+    if (this->operator==(other) || this->operator>(other))
+        return true;
+    else
+        return false;
+}
+
+bool DateTime::operator<=(const mono::DateTime &other) const
+{
+    if (this->operator==(other) || this->operator<(other))
+        return true;
+    else
+        return false;
+}
+
+// MARK: Static Public Methods
+
+bool DateTime::isLeapYear(uint16_t year)
+{
+    return year % 400 == 0 || (year % 4 == 0  && year % 100 != 0);
+}
+
+DateTime DateTime::fromISO8601(mono::String date)
+{
+    Regex isoReg("(\\d\\d\\d\\d)-(\\d\\d)-(\\d\\d)[T ](\\d\\d):(\\d\\d):(\\d\\d)(Z)?");
+    Regex::Capture cap[8];
+
+    if (!isoReg.Match(date, cap, 8))
+        return DateTime();
+
+    TimeTypes zone = UNKNOWN_TIME_ZONE;
+
+    int year, month, day, hour, minute, second;
+    sscanf(isoReg.Value(cap[0])(), "%d", &year);
+    sscanf(isoReg.Value(cap[1])(), "%d", &month);
+    sscanf(isoReg.Value(cap[2])(), "%d", &day);
+
+    sscanf(isoReg.Value(cap[3])(), "%d", &hour);
+    sscanf(isoReg.Value(cap[4])(), "%d", &minute);
+    sscanf(isoReg.Value(cap[5])(), "%d", &second);
+
+    if (isoReg.Value(cap[6]) == String("Z"))
+        zone = UTC_TIME_ZONE;
+//    else if (isoReg.Value(cap[8]).Length() > 0)
+//    {
+//        int offset = 0;
+//        sscanf(isoReg.Value(cap[8])(), "%d", &offset);
+//        DateTime utcTime(year, month, day, hour, minute, second, UTC_TIME_ZONE);
+//        return utcTime.addHours(-offset);
+//    }
+
+    return DateTime(year, month, day, hour, minute, second, zone);
+}
 
 // MARK: Protected internal methods
+
+void DateTime::incrementSecond()
+{
+    secs++;
+    if (secs > 59)
+    {
+        secs = 0;
+        incrementMinute();
+    }
+}
+
+void DateTime::incrementMinute()
+{
+    mins++;
+    if (mins > 59)
+    {
+        mins = 0;
+        incrementHour();
+    }
+}
+
+void DateTime::incrementHour()
+{
+    hours++;
+    if (hours > 23)
+    {
+        hours = 0;
+        incrementDayOfMonth();
+    }
+}
+
+void DateTime::incrementDayOfMonth()
+{
+    day++;
+    if (leapYear && month == 2)
+    {
+        if (day > 29)
+        {
+            day = 1;
+            incrementMonth();
+        }
+    }
+    else if (day > DaysPerMonth[month])
+    {
+        day = 1;
+        incrementMonth();
+    }
+}
+
+void DateTime::incrementMonth()
+{
+    month++;
+    if (month > 12)
+    {
+        month = 1;
+        incrementYear();
+    }
+}
+
+void DateTime::incrementYear()
+{
+    year++;
+    leapYear = isLeapYear(year);
+}
 
 uint8_t DateTime::trim(uint8_t value, uint8_t min, uint8_t max)
 {
@@ -42,4 +370,24 @@ uint8_t DateTime::trim(uint8_t value, uint8_t min, uint8_t max)
         return max;
 
     return value;
+}
+
+/// MARK: STATIC SYSTEM DATETIME
+
+int DateTime::LocalTimeZoneHourOffset = 0;
+DateTime DateTime::systemDateTimeClock = DateTime(1970, 1, 1, 0, 0, 0, DateTime::UTC_TIME_ZONE);
+
+void DateTime::incrementSystemClock()
+{
+    systemDateTimeClock.incrementSecond();
+}
+
+DateTime DateTime::now()
+{
+    return systemDateTimeClock;
+}
+
+void DateTime::setSystemDateTime(mono::DateTime dt)
+{
+    systemDateTimeClock = dt;
 }
--- src/date_time.h
+++ src/date_time.h
@@ -10,17 +10,60 @@
 namespace mono {
 
     /**
-     * @brief A DateTime representation
+     * @brief A Date and time representation in the Gregorian calendar
      * 
+     * This class represents a point in time, defined in the gregorian calendar.
+     * Such a timestamp are given in year, month, day of month, hours since
+     * midnight, minutes and seconds. This class also defined if the timestamp
+     * is in UTC / GMT or a defined local time zone.
      *
+     * The class handles leap years and the varying length of months. You can
+     * add seconds, minutes, hours and days to a DateTime object and get the
+     * result as a new DateTime object.
+     *
+     * When you create DateTime objects they are created in the local time zone
+     * by default. The local time zone is defined as a offset in hours relative
+     * to UTC. There is no notion of IANA Time Zone names of alike - just an
+     * offset to the UTC time.
+     *
+     * There are two convenient method to print DateTime as readable strings.
+     * The @ref toString method print a human readable (MySql compatible)
+     * timestamp. The other @ref toISO8601 returns a string formatted in the
+     * ISO 8601 standard format used in JSON objects.
+     *
+     * When printing DateTime objects, they are returned in the time zone that
+     * they are created in.
+     *
+     * ### System Wall Clock
+     *
+     * This class also has a global DateTime object reserved for use by a RTC
+     * feature. A subsystem manages the RTC and increments the global system
+     * DateTime object.
+     *
+     * You can get the current DateTime time by using the static method @ref now
+     * To set the system clock use the static method @ref setSystemClock
      */
     class DateTime
     {
+    public:
+
+        /** DateTime timestamps can be one of three types */
+        enum TimeTypes {
+            LOCAL_TIME_ZONE,    /**< The DateTime is specified in local time zone */
+            UTC_TIME_ZONE,      /**< The DateTime is specified in UTC / GMT time zone */
+            UNKNOWN_TIME_ZONE   /**< The DateTime do not have a specified time zone */
+        };
+
+        static const uint8_t DaysPerMonth[13];
+        
     protected:
         // MARK: Protected Members
 
         int8_t secs, mins, hours, day, month;
         uint16_t year;
+        bool leapYear;
+        TimeTypes type;
+
     public:
         // MARK: Public Contructors
 
@@ -39,9 +82,15 @@ namespace mono {
          * @param hours Optional: The hour component of the timestamp, range is 0 to 23
          * @param minutes The minute component of the timestamp, range is 0 to 59
          * @param seconds The seconds component of the timestamp, range is 0 to 59
+         * @param zone The timezone where this DateTime define its time, default is the local timezone
          */
         DateTime(uint16_t years, uint8_t months, uint8_t days,
-                 uint8_t hours = 0, uint8_t minutes = 0, uint8_t seconds = 0);
+                 uint8_t hours = 0, uint8_t minutes = 0, uint8_t seconds = 0,
+                 TimeTypes zone = LOCAL_TIME_ZONE);
+
+        DateTime(const DateTime &other);
+
+        DateTime &operator=(const DateTime &other);
 
 
         // MARK: Public Methods
@@ -50,13 +99,87 @@ namespace mono {
          * @brief Return the DateTime object as a huamn readable string
          * @return A mono string on the format: yyyy-MM-dd hh:mm:ss
          */
-        String ToString() const;
+        String toString() const;
 
+        /**
+         * @brief Return an ISO8601 formatted timestamp as a string
+         */
+        String toISO8601() const;
+
+
+        bool isValid() const;
+
+        /** @brief Convert this DateTime to UTC time */
+        DateTime toUtcTime() const;
+
+        // MARK: Operator Overloads
+
+        bool operator==(const DateTime &other) const;
+        bool operator!=(const mono::DateTime &other) const;
+
+        bool operator>=(const mono::DateTime &other) const;
+        bool operator>(const mono::DateTime &other) const;
+        bool operator<(const mono::DateTime &other) const;
+        bool operator<=(const mono::DateTime &other) const;
+
+        // MARK: Date / Time manipulation
+        
+        DateTime addSeconds(int seconds) const;
+        DateTime addMinutes(int minutes) const;
+        DateTime addHours(int hours) const;
+        DateTime addDays(int days) const;
+//        DateTime addMonths(int months) const;
+//        DateTime addYears(int years) const;
+
+        // MARK: Static Methods
+
+        /** Get the maximum possible DateTime value (far in the future) */
+        static DateTime maxValue() { return DateTime(9999, 12, 31, 23, 59, 59); }
+
+        /** Get the lowest possible DateTime value (the distant past) */
+        static DateTime minValue() { return DateTime(0, 1, 1, 00, 0, 0); }
+
+        /** @brief Check is a year is a leap year */
+        static bool isLeapYear(uint16_t year);
+
+        /**
+         * @brief Parse a subset of ISO 8601 compatible date time representations
+         *
+         * This static method takes a ISO 8601 formatted string, and creates a
+         * DateTime object from that. This method only parses a subset of the
+         * possible date representations allowed in ISO 8601. Specifically
+         * it can handle dates in these format:
+         *
+         * * `yyyy-MM-ddTHH:mm:ssZ`
+         * * `yyyy-MM-ddTHH:mm:ss+01:00` or other time zones
+         * * `yyyy-MM-dd HH:mm:ssZ`
+         * * `yyyy-MM-dd HH:mm:ss`
+         *
+         * @return The parsed DateTime object, that might be valid or invalid
+         */
+        static DateTime fromISO8601(String date);
 
     protected:
         // MARK: Protected internal methods
 
+        void incrementSecond();
+        void incrementMinute();
+        void incrementHour();
+        void incrementDayOfMonth();
+        void incrementMonth();
+        void incrementYear();
+
         inline uint8_t trim(uint8_t value, uint8_t min, uint8_t max);
+
+        static DateTime systemDateTimeClock;
+        static void incrementSystemClock();
+
+    public:
+
+        static int LocalTimeZoneHourOffset;
+
+        static void setSystemDateTime(DateTime dt);
+        static DateTime now();
     };
 
 }
