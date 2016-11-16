9b3b594e362fc5a7296e4f8b9b24ac7936f904d8
Kristoffer Andersen <stoffera@gmail.com>
2016-11-16 21:35:44 +0100

    added test case files

---
+++ src/tests/date_time_test.cpp
@@ -0,0 +1,88 @@
+//
+//  main.cpp
+//  dtTest
+//
+//  Created by Kristoffer Lyder Andersen on 14/11/2016.
+//  Copyright © 2016 Kristoffer Lyder Andersen. All rights reserved.
+//
+
+#define SLRE_DEBUG 1
+#define CATCH_CONFIG_MAIN
+#include "../../../../catch.hpp"
+#include "../../../src/date_time.h"
+
+using namespace mono;
+
+SCENARIO("DateTime and calendar works")
+{
+    GIVEN("A specific date")
+    {
+        DateTime today(2016, 11, 14);
+        printf("Today %s\n", today.toString()());
+
+        WHEN("equal comparison must work")
+        {
+            DateTime other(2016, 11, 14);
+            REQUIRE(today == other);
+            REQUIRE(today >= other);
+            REQUIRE(today <= other);
+        }
+
+        WHEN("not equal must work")
+        {
+            DateTime other(2016, 10, 31);
+            printf("other: %s\n",other.toUtcTime().toISO8601()());
+            REQUIRE(today != other);
+            REQUIRE(today > other);
+            REQUIRE(today >= other);
+            REQUIRE(other < today);
+            REQUIRE(other <= today);
+        }
+
+        WHEN("1 day is added")
+        {
+            DateTime tomorrow = today.addDays(1);
+            printf("tomorrow: %s\n", tomorrow.toString()());
+
+            REQUIRE(today < tomorrow);
+        }
+
+        REQUIRE(DateTime::isLeapYear(1970) == false);
+        REQUIRE(DateTime::isLeapYear(1972) == true);
+
+        WHEN("1000 days is added to EPOCH")
+        {
+            DateTime epoch(1970,1,1,0,0,0,DateTime::UTC_TIME_ZONE);
+            printf("Epoch: %s\n",epoch.toString()());
+
+            DateTime later = epoch.addDays(31);
+            printf("31 days later: %s\n", later.toString()());
+
+            later = epoch.addDays(61);
+            printf("61 days later: %s\n", later.toString()());
+
+            later = epoch.addDays(365);
+            printf("365 days later: %s\n", later.toString()());
+
+            later = epoch.addDays(365*2);
+            printf("730 days later: %s\n", later.toString()());
+
+            later = epoch.addDays(365*3);
+            printf("1095 days later: %s\n", later.toString()());
+        }
+
+        WHEN("we parse a ISO date")
+        {
+            String dateStr("2016-11-14T23:56:12Z");
+            String dateStr2("2016-11-14T23:56:12+01:00");
+            String dateStr3("2016-11-14 23:56:12.000+01:30");
+
+            DateTime d1 = DateTime::fromISO8601(dateStr);
+            printf("D1: %s\n", d1.toString()());
+            DateTime d2 = DateTime::fromISO8601(dateStr2);
+            printf("D2: %s\n", d2.toString()());
+            DateTime d3 = DateTime::fromISO8601(dateStr3);
+            printf("D3: %s\n", d3.toString()());
+        }
+    }
+}
---
+++ src/tests/http_client_test.cpp
@@ -0,0 +1,193 @@
+//
+//  http_client_test.cpp
+//  httpClientTest
+//
+//  Created by Kristoffer Andersen on 29/08/2016.
+//  Copyright © 2016 Monolit ApS. All rights reserved.
+//
+
+#define SLRE_DEBUG 1
+
+#include "../../catch.hpp"
+#include "../../../mn_string.h"
+#include "../../../regex.h"
+
+using mono::String;
+using mono::Regex;
+
+SCENARIO("Check that the HTTPClient can parse URLs")
+{
+    GIVEN("A new a IP addr regex")
+    {
+        mono::Regex regex("(http://)(\\d+\\.\\d+\\.\\d+\\.\\d+):?(\\d*)(/?[^\\s'\\\"<>]*)");
+
+        WHEN("the an IPv4 is given (without port num.): http://10.0.41.190")
+        {
+
+            String input("http://10.0.41.190");
+            Regex::Capture capture[4];
+            bool match = regex.Match(input, capture, 4);
+
+            THEN("Ip should match regex!")
+            {
+                REQUIRE(match == true);
+            }
+
+            THEN("The params should be correct")
+            {
+
+                REQUIRE(regex.Value(capture[0]) == "http://");
+                REQUIRE(regex.Value(capture[1]) == "10.0.41.190");
+                REQUIRE(regex.Value(capture[2]).Length() == 0);
+                REQUIRE(regex.Value(capture[3]) == "");
+            }
+        }
+
+        WHEN("we use a IP addr with path")
+        {
+            String input("http://10.0.41.190/hej/med/dig?options=sort&test=false&pageSize=56");
+            Regex::Capture capture[4];
+            bool match = regex.Match(input, capture, 4);
+
+            THEN("Ip should match regex!")
+            {
+                REQUIRE(match == true);
+            }
+
+            THEN("The params should be correct")
+            {
+                REQUIRE(regex.Value(capture[0]) == "http://");
+                REQUIRE(regex.Value(capture[1]) == "10.0.41.190");
+                REQUIRE(regex.Value(capture[2]).Length() == 0);
+                REQUIRE(regex.Value(capture[3]) == "/hej/med/dig?options=sort&test=false&pageSize=56");
+            }
+        }
+
+        WHEN("we use a IP addr with port num")
+        {
+            String input("http://10.0.41.190:8080");
+            Regex::Capture capture[4];
+            bool match = regex.Match(input, capture, 4);
+
+            THEN("Ip should match regex!")
+            {
+                REQUIRE(match == true);
+            }
+
+            THEN("The params should be correct")
+            {
+                REQUIRE(regex.Value(capture[0]) == "http://");
+                REQUIRE(regex.Value(capture[1]) == "10.0.41.190");
+                REQUIRE(regex.Value(capture[2]) == "8080");
+                REQUIRE(regex.Value(capture[3]) == "");
+            }
+        }
+
+        WHEN("we use a IP addr with port num and path")
+        {
+            String input("http://10.0.41.190:8080/hej/med/dig?options=sort&test=false&pageSize=56");
+            Regex::Capture capture[4];
+            bool match = regex.Match(input, capture, 4);
+
+            THEN("Ip should match regex!")
+            {
+                REQUIRE(match == true);
+            }
+
+            THEN("The params should be correct")
+            {
+                REQUIRE(regex.Value(capture[0]) == "http://");
+                REQUIRE(regex.Value(capture[1]) == "10.0.41.190");
+                REQUIRE(regex.Value(capture[2]) == "8080");
+                REQUIRE(regex.Value(capture[3]) == "/hej/med/dig?options=sort&test=false&pageSize=56");
+            }
+        }
+    }
+
+    GIVEN("A new a domain based address regex")
+    {
+        mono::Regex regex("(http://)([^\\s/'\\\"<>\\?:,_;\\*\\^\\!<>]+):?(\\d*)(/?[^\\s'\\\"<>]*)");
+
+        WHEN("a domain is given (without port num.): http://www.google.com")
+        {
+
+            String input("http://www.google.com");
+            Regex::Capture capture[4];
+            bool match = regex.Match(input, capture, 4);
+
+            THEN("Ip should match regex!")
+            {
+                REQUIRE(match == true);
+            }
+
+            THEN("The params should be correct")
+            {
+
+                REQUIRE(regex.Value(capture[0]) == "http://");
+                REQUIRE(regex.Value(capture[1]) == "www.google.com");
+                REQUIRE(regex.Value(capture[2]).Length() == 0);
+                REQUIRE(regex.Value(capture[3]).Length() == 0);
+            }
+        }
+
+        WHEN("we use a domain with path")
+        {
+            String input("http://www.google.com/hej/med/dig?options=sort&test=false&pageSize=56");
+            Regex::Capture capture[4];
+            bool match = regex.Match(input, capture, 4);
+
+            THEN("Ip should match regex!")
+            {
+                REQUIRE(match == true);
+            }
+
+            THEN("The params should be correct")
+            {
+                REQUIRE(regex.Value(capture[0]) == "http://");
+                REQUIRE(regex.Value(capture[1]) == "www.google.com");
+                REQUIRE(regex.Value(capture[2]).Length() == 0);
+                REQUIRE(regex.Value(capture[3]) == "/hej/med/dig?options=sort&test=false&pageSize=56");
+            }
+        }
+
+        WHEN("we use a domain with port num")
+        {
+            String input("http://www.google.com:8080");
+            Regex::Capture capture[4];
+            bool match = regex.Match(input, capture, 4);
+
+            THEN("Ip should match regex!")
+            {
+                REQUIRE(match == true);
+            }
+
+            THEN("The params should be correct")
+            {
+                REQUIRE(regex.Value(capture[0]) == "http://");
+                REQUIRE(regex.Value(capture[1]) == "www.google.com");
+                REQUIRE(regex.Value(capture[2]) == "8080");
+                REQUIRE(regex.Value(capture[3]).Length() == 0);
+            }
+        }
+
+        WHEN("we use a domain with port num and path")
+        {
+            String input("http://www.google.com:8080/hej/med/dig?options=sort&test=false&pageSize=56");
+            Regex::Capture capture[4];
+            bool match = regex.Match(input, capture, 4);
+
+            THEN("Ip should match regex!")
+            {
+                REQUIRE(match == true);
+            }
+
+            THEN("The params should be correct")
+            {
+                REQUIRE(regex.Value(capture[0]) == "http://");
+                REQUIRE(regex.Value(capture[1]) == "www.google.com");
+                REQUIRE(regex.Value(capture[2]) == "8080");
+                REQUIRE(regex.Value(capture[3]) == "/hej/med/dig?options=sort&test=false&pageSize=56");
+            }
+        }
+    }
+}
